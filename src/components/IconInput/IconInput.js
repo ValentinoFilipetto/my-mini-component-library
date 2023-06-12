cd6503208227bd6dc93a8bd23c9427a7b1107208
Valentino Filipetto <valentino.filipetto@finaps.nl>
2023-06-12 14:58:18 +0200

    style: add final version of select and first of icon input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
 
-import { COLORS } from '../../constants';
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  let Component;
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
-
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+  if (size === "small") {
+    Component = SmallSearch;
+  } else if (size === "large") {
+    Component = LargeSearch;
+  }
+  return (
+    <Component style={{ "--width": width + "px" }}>
+      <Icon id={icon} strokeWidth={1} size={24} />
+      <Label>{placeholder}</Label>
+    </Component>
+  );
 };
+
+const SmallSearch = styled.div`
+  position: relative;
+  width: var(--width);
+  font-size: 1rem;
+  border-bottom: 1px solid #000000;
+  color: ${COLORS.gray500};
+`;
+
+const LargeSearch = styled.div`
+  position: relative;
+  width: var(--width);
+  font-size: var(--size);
+  border-bottom: 2px solid #000000;
+  color: ${COLORS.gray700};
+`;
+
+const Label = styled.label`
+  position: absolute;
+  top: 0;
+  left: 33px;
+  top: 4px;
+`;
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,58 @@
-import React from 'react';
-import styled from 'styled-components';
-
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import React from "react";
+import styled from "styled-components";
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <SelectBar value={value} onChange={onChange}>
+        {children}
+      </SelectBar>
+      <DisplayedPart>
+        {displayedValue}
+        <IconWrapper style={{ "--height": 24 + "px" }}>
+          <Icon id="chevron-down" strokeWidth={1} size={24} />
+        </IconWrapper>
+      </DisplayedPart>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const SelectBar = styled.select`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const DisplayedPart = styled.div`
+  background: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  border-radius: 8px;
+  padding: 12px 16px;
+  padding-right: 52px;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  right: 10px;
+  bottom: 0;
+  margin: auto;
+  height: var(--height);
+`;
 
 export default Select;
